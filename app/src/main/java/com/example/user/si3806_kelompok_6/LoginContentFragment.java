27139a1d26d8f46291bdae391204997d660d71d4
anisanurazizah <nurazizahanisa5@gmail.com>
2017-03-04 09:34:32 +0700

    Tampilan Login
    Tampilan Login selesai

--- app/src/main/java/com/example/user/si3806_kelompok_6/LoginContentFragment.java
+++ app/src/main/java/com/example/user/si3806_kelompok_6/LoginContentFragment.java
@@ -6,12 +6,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginContentFragment extends Fragment implements View.OnClickListener {
 
+    private Button mLogin;
+    private TextView mRegis;
+    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.item_login, container,false);
+        mRegis = (TextView) view.findViewById(R.id.link_signup);
+        mRegis.setOnClickListener(LoginContentFragment.this);
+
         return view;
     }
 
@@ -20,3 +28,4 @@ public class LoginContentFragment extends Fragment implements View.OnClickListen
 
     }
 }
+
